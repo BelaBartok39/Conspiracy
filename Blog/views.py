ae78a7476e35b33325e8ce081ba69b92b33b1723
babynicky <jackthelion83@gmail.com>
2023-06-11 06:47:59 -0500

    Profile Update

--- Blog/views.py
+++ Blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
 # Create your views here.
 
@@ -32,6 +33,15 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = "posts"
+    ordering = ['-date_posted']
+    paginate_by = 5
+
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
 
+
+    
